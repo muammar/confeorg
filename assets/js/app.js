ee4d2694dd3d258d4e82e595a45af1db431c2cb2
Claudia El Khatib <claudiacv22@gmail.com>
2019-08-06 12:55:03 -0700

    Revert "Adición de un carousel"
    This reverts commit 2d07e7497f03beff048b6f2f2708abc00e71712b.

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,14 +23,3 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
-
-// Initialise FlexSlider for Carousels
-$(window).load(function() {
-  $('.flexslider').flexslider({
-  animation: "fade",
-  directionNav: true,
-  slideshowSpeed: 5000,
-  animationSpeed: 600,
-  touch: true
-  });
-});
